21efa59cf79bb4ba917064a972dddbb71fa49986
John Herrin <jherrinva@gmail.com>
2017-02-04 21:46:29 -0500

    Updated Interpreter sampledungeonmethod
    Updated interpreters sampledungeon to build a small dungeon
    
    Updated main method to test this method

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -8,25 +8,46 @@ import java.util.Scanner;
 
 public class Interpreter 
 {
-    public static void main (String[] args)
+    
+    private static Dungeon buildSampleDungeon()
     {
+        Room zietzDungeonEntry = new Room("Zietz Dungeon Entry");
+        zietzDungeonEntry.setDesc("This room is very hot! Find the thermostat");
+        Dungeon sampleDungeon = new Dungeon(zietzDungeonEntry,"Zietz Dungeon");
+        
+        return sampleDungeon;
+    }
+    
+    
+    
+    public static void main (String[] args)
+    {   
+        
+        Dungeon startingDungeon = buildSampleDungeon();
+        System.out.println("Welcome to " + startingDungeon.getName() + ".  You are in " + startingDungeon.getEntry().getTitle() + ". " +  startingDungeon.getEntry().describe());
+        
+        
+        /////////Sample query below/////////
+        /**
         Scanner in = new Scanner(System.in);
-        
         String userInput = new String();
-        
         while (!userInput.equals("q"))
         {
             System.out.println("What would you like to do? : ");
             userInput = in.nextLine();
         }
-        
+        */
+        /////////////////////////////////////
         
         
         //////////Test room below////////////
+        /**
         Room newRoom = new Room("Dr Zietz Room");
         newRoom.setDesc("This room is very hot.  Find the thermostat!");
-        newRoom.describe()
-        
+        System.out.println(newRoom.describe());
+        System.out.println(newRoom.describe());
+        */
         ///////////////////////////////////////////
     }
+    
 }
