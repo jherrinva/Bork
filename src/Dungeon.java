237d49130de66feed1acf2d136fb5075985c7854
John Herrin <jherrinva@gmail.com>
2017-02-15 13:59:38 -0500

    Added new Dungeon constructor.  Now takes in string containing fileName to process
    Added comments describing what constructor will do.  Will be coded later
    
    Changed old constructor to protected
    
    Added sample .bork and .sav files to source directory.  Will be acounted for later

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -18,11 +18,28 @@ public class Dungeon
      * @param entry room to be used as entry point
      * @param name Name of actual dungeon
      */
-    public Dungeon(Room entry, String name)
+    protected Dungeon(Room entry, String name)
     {
         this.name = name;
         this.dungeonEntry = entry;
         roomCollection.put(this.dungeonEntry.getTitle(),this.dungeonEntry);
+        
+    }
+   
+    
+    public Dungeon(String fileName)
+    {
+        //needs to read the first line of .bork and use it as dungeon name
+        //second line needs to have the literal line "Bork v2.0" exactly.
+        //if match, needs to throw away the line and continue
+        //if no match, needs to print an error message indicating dungeon file is not 
+        //compatible, and exit the program
+        //third line will be === dilimiter, this is when room construction begins
+        //4th line is "Rooms:"    throw this away to
+        // first line of each room will be room title
+        //all following lines will be room description until --- is found, which denotes next room
+        //continues until === is found which begins exits
+        
         
     }
     
